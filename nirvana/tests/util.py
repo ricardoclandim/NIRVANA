6e9feca4da0b4ebd53213c820ffcab613ecac12a
Kyle Westfall <westfall@ucolick.org>
2022-05-06 07:27:06 -0700

    test fixes

--- nirvana/tests/util.py
+++ nirvana/tests/util.py
@@ -35,8 +35,10 @@ def remote_dap_test_files(daptype='HYB10-MILESHC-MASTARHC2'):
     return ['manga-8138-12704-MAPS-{0}.fits.gz'.format(daptype),
             'manga-8078-12703-MAPS-{0}.fits.gz'.format(daptype)]
 
-drp_test_version = 'v3_0_1'
-dap_test_version = '3.0.1'
+#drp_test_version = 'v3_0_1'
+#dap_test_version = '3.0.1'
+drp_test_version = 'v3_1_1'
+dap_test_version = '3.1.0'
 dap_test_daptype = 'HYB10-MILESHC-MASTARHC2'
 
 test_files = remote_drp_test_files() + remote_dap_test_files(daptype=dap_test_daptype)
